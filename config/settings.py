904730bf24abf7f44da8e9f64028d959d185dddc
amalGuesmii <amal.guesmi@umanlink.com>
2026-09-16 22:00:58 +0100

    fix: update ALLOWED_HOSTS and CORS_ALLOWED_ORIGINS for local development

--- config/settings.py
+++ config/settings.py
@@ -26,8 +26,10 @@ SECRET_KEY = 'django-insecure-ud&%oh)ota!5f@q^q+c!aycsl-+mhpg4goaz^&pj8g^&ejei^v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8001',
+    'http://127.0.0.1:8001',
     'http://localhost:4200',
     'http://127.0.0.1:4200',
     'http://localhost:8100',
